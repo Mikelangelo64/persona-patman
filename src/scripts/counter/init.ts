74c48c762d61455469c2acb5493cb4919cb2cdbe
Mikelangelo64 <donni2001331@gmail.com>
2023-08-28 15:20:18 +0300

    add footer

--- src/scripts/counter/init.ts
+++ src/scripts/counter/init.ts
@@ -1,30 +1,72 @@
 import { Timeline } from 'vevet';
+import useObserver from '../config/useObserver';
 
-const counterInit = () => {
-  const counterArray: NodeListOf<HTMLElement> =
-    document.querySelectorAll('.counter');
+interface IMakeCounter {
+  item: HTMLElement;
+  container: HTMLElement;
+}
 
-  if (counterArray.length === 0) {
+interface IMakeTimeline {
+  item: HTMLElement;
+}
+
+const makeTimeline: (props: IMakeTimeline) => Timeline | undefined = ({
+  item: itemProp
+}) => {
+  const item = itemProp;
+
+  const value = +(item.dataset.value || 0);
+  if (Number.isNaN(value)) {
+    return undefined;
+  }
+
+  const timeline = new Timeline({ duration: 4000, destroyOnEnd: true });
+
+  timeline.addCallback('progress', ({ easing }) => {
+    item.innerHTML = `${Math.floor(value * easing)}`;
+  });
+
+  timeline.addCallback('end', () => {
+    item.classList.add('finished');
+  });
+
+  return timeline;
+};
+
+const makeCounter: (props: IMakeCounter) => void = ({ item, container }) => {
+  const timeline = makeTimeline({ item });
+
+  if (!timeline) {
     return;
   }
 
-  counterArray.forEach((itemProp) => {
-    const item = itemProp;
-    const value = +(item.dataset.value || 0);
-    if (Number.isNaN(value)) {
+  useObserver({
+    target: container,
+    isCallOnce: true,
+    callbackIn: () => {
+      timeline.play();
+    }
+  });
+};
+
+const counterInit = () => {
+  const containerArray: NodeListOf<HTMLElement> =
+    document.querySelectorAll('.counter-container');
+  if (containerArray.length === 0) {
+    return;
+  }
+
+  containerArray.forEach((container) => {
+    const counterArray: NodeListOf<HTMLElement> =
+      container.querySelectorAll('.counter');
+
+    if (counterArray.length === 0) {
       return;
     }
 
-    const timeline = new Timeline({ duration: 4000, destroyOnEnd: true });
-
-    timeline.addCallback('progress', ({ easing }) => {
-      item.innerHTML = `${Math.floor(value * easing)}`;
+    counterArray.forEach((item) => {
+      makeCounter({ item, container });
     });
-
-    timeline.addCallback('end', () => {
-      item.classList.add('finished');
-    });
-    timeline.play();
   });
 };
 
